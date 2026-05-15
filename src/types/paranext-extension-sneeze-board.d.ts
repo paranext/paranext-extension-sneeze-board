3a046675caa7f1df98f8efb2cf61099bfc611ca3
tjcouch-sil <tj_couch@sil.org>
2026-05-14 23:41:21 -0500

    feat(types): declare sneeze board state types

--- src/types/paranext-extension-sneeze-board.d.ts
+++ src/types/paranext-extension-sneeze-board.d.ts
@@ -1,4 +1,25 @@
 declare module 'paranext-extension-sneeze-board' {
-  // Add extension types exposed on the papi for other extensions to use here
-  // More instructions can be found in the README
+  export type SneezeRecord = { userId: string; date: string; comment?: string };
+  export type UserInfo = { userId: string; color: string; name: string };
+  export type SneezeDatabase = {
+    version: number;
+    countdownStart: number;
+    sneezes: SneezeRecord[];
+    users: UserInfo[];
+  };
+
+  export type ConnectionState = 'idle' | 'connecting' | 'open' | 'closed' | 'error';
+
+  export type SneezeBoardState = {
+    connection: ConnectionState;
+    error?: string;
+    database?: SneezeDatabase;
+    currentUserId?: string;
+  };
+
+  /** PAPI NetworkObject contract for the Sneeze Board */
+  export type SneezeBoardStateNetworkObject = {
+    getState(): Promise<SneezeBoardState>;
+    subscribeState(callback: (state: SneezeBoardState) => void): Promise<() => Promise<boolean>>;
+  };
 }
